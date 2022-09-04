dbed63791e63affaa907b2bdf0dc90fb93ba1824
W1llH <whemsley12@gmail.com>
2022-09-04 13:28:10 +0100

    minor changes

--- mandelbrot.h
+++ mandelbrot.h
@@ -5,12 +5,12 @@
 #include <complex.h>
 #include "bmp.h"
 
-double cmag (double complex z) {
+double cmag (const double complex z) {
     double magnitude = sqrt(pow(creal(z),2)+pow(cimag(z),2));
     return magnitude;
 }
 
-double mandItResult (double complex c, unsigned int *no_of_it) {
+double mandItResult (const double complex c, const unsigned int *no_of_it) {
     if (cmag(c) < 2) {
         unsigned char *i = (unsigned char *)malloc(sizeof(unsigned short)); // limits possible iterations to 255 for now
         *i = 0;
@@ -31,7 +31,7 @@ double mandItResult (double complex c, unsigned int *no_of_it) {
     }
 }
 
-struct rgb_data *MandBMPpixels (double imgw, double imgh, double min_x, double max_x, double min_y, double max_y, unsigned int *iterations, struct rgb_data *pixels){;
+struct rgb_data *MandBMPpixels (double imgw, double imgh, double min_x, double max_x, double min_y, double max_y, const unsigned int *iterations, struct rgb_data *pixels){;
     double xIncr = (max_x - min_x)/imgw;
     double yIncr = (max_y - min_y)/imgh;
 
